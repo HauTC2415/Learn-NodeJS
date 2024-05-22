95245351cd5cede6105ba271f293ed3f47d11797
truongconghau <truongconghau@truongconghaus-Mac-mini.local>
2024-05-22 10:09:57 +0700

    hash password before add into db

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,6 +1,7 @@
 import User from '~/models/schemas/User.schema'
 import databaseService from './database.services'
 import { RegisterRequestBody } from '~/models/requests/User.requests'
+import { hashSha256 } from '~/utils/crypto'
 
 class UsersService {
   async login(data: { email: string; password: string }) {
@@ -16,7 +17,7 @@ class UsersService {
   }
 
   async register(data: RegisterRequestBody) {
-    const user = new User({ ...data, date_of_birth: new Date(data.date_of_birth) })
+    const user = new User({ ...data, date_of_birth: new Date(data.date_of_birth), password: hashSha256(data.password) })
     const rs = await databaseService.users.insertOne(user)
     return rs
   }
